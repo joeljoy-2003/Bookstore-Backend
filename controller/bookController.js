7f418fa9a412f818c10a0b1dd885c0edce9e475b
joeljoy-2003 <joeljoy43240@gmail.com>
2025-12-11 17:45:08 +0530

    stripe url updated

--- controller/bookController.js
+++ controller/bookController.js
@@ -197,8 +197,10 @@ exports.makeBookPaymentController = async (req, res) => {
             payment_method_types: ["card"],
             line_items,
             mode: 'payment',
-            success_url: 'http://localhost:5173/payment-success',
-            cancel_url: "http://localhost:5173/payment-error",
+          //  success_url: 'http://localhost:5173/payment-success',
+           // cancel_url: "http://localhost:5173/payment-error",
+            success_url: 'https://bookstore-frontend-snowy-sigma.vercel.app/payment-success',
+            cancel_url: "https://bookstore-frontend-snowy-sigma.vercel.app/payment-error",
 
         });
         console.log(session);
